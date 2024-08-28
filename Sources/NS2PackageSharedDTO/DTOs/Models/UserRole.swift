1cd31bb2de7c3887ddc00cdb1de3432682009d7f
Jaled Jara <jaledjara123@outlook.com>
2024-08-28 11:46:29 -0300

    UserRole

---
+++ Sources/NS2PackageSharedDTO/DTOs/Models/UserRole.swift
@@ -0,0 +1,13 @@
+//
+//  UserRole.swift
+//
+//
+//  Created by Jaled Jara on 8/28/24.
+//
+
+import Foundation
+
+public enum UserRole: String, Codable {
+    case admin
+    case user
+}
--- Sources/NS2PackageSharedDTO/DTOs/Request/RegisterRequestDTO.swift
+++ Sources/NS2PackageSharedDTO/DTOs/Request/RegisterRequestDTO.swift
@@ -12,12 +12,14 @@ public struct RegisterRequestDTO: Codable {
     public let password: String
     public let phoneNumber: String
     public let addresses: [AddressRequestDTO]
+    public let role: UserRole // Añadido el rol aquí
     
-    public init(email: String, username: String, password: String, phoneNumber: String, addresses: [AddressRequestDTO]) {
+    public init(email: String, username: String, password: String, phoneNumber: String, addresses: [AddressRequestDTO], role: UserRole) {
         self.email = email
         self.username = username
         self.password = password
         self.phoneNumber = phoneNumber
         self.addresses = addresses
+        self.role = role
     }
 }
