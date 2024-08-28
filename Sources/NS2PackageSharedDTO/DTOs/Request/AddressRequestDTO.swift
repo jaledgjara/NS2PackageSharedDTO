297d88ff3495ef22536561ab1effed46aaeacfa6
Jaled Jara <jaledjara123@outlook.com>
2024-08-28 15:13:47 -0300

    cut numeration

--- Sources/NS2PackageSharedDTO/DTOs/Request/AddressRequestDTO.swift
+++ Sources/NS2PackageSharedDTO/DTOs/Request/AddressRequestDTO.swift
@@ -11,12 +11,10 @@ public struct AddressRequestDTO: Codable {
     public let street: String
     public let state: String
     public let zipCode: String
-    public let numeration: String
     
-    public init(street: String, state: String, zipCode: String, numeration: String) {
+    public init(street: String, state: String, zipCode: String) {
         self.street = street
         self.state = state
         self.zipCode = zipCode
-        self.numeration = numeration
     }
 }
