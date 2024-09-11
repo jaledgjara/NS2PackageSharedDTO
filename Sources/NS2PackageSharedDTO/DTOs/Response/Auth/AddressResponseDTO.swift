c5b91c614cf0445975eed405235c9047a3219476
Jaled Jara <jaledjara123@outlook.com>
2024-09-11 13:38:56 -0300

    ResponseDTOADDRESS

---
+++ Sources/NS2PackageSharedDTO/DTOs/Response/Auth/AddressResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  AddressResponseDTO.swift
+//  
+//
+//  Created by Jaled Jara on 9/11/24.
+//
+
+import Foundation
+
+public struct AddressResponseDTO: Codable {
+    public var id: UUID?
+    public let street: String
+    public let city: String
+    public let state: String
+    public let zipCode: String
+
+    public init(id: UUID? = nil, street: String, city: String, state: String, zipCode: String) {
+        self.id = id
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+    }
+}
