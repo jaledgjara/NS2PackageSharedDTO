ddba434ad87f18e29e9139f5ed27a8a4760d3978
Jaled Jara <jaledjara123@outlook.com>
2024-09-16 13:06:38 -0300

    MercadoPagoDTOs

---
+++ Sources/NS2PackageSharedDTO/DTOs/Response/MercadoPago/MercadoPagoResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  MercadoPagoResponseDTO.swift
+//
+//
+//  Created by Jaled Jara on 9/16/24.
+//
+
+import Foundation
+
+public struct MercadoPagoResponseDTO: Codable, Hashable {
+    public let id: String
+    public let status: String
+    public let statusDetail: String
+    public let initPoint: String
+
+    public init(id: String, status: String, statusDetail: String, initPoint: String) {
+        self.id = id
+        self.status = status
+        self.statusDetail = statusDetail
+        self.initPoint = initPoint
+    }
+}
